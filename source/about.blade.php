c769f9e42823817c1234cfaeb5778ffe924bccea
breadthe <ferdinand.bismarck@gmail.com>
2019-01-27 19:07:37 -0600

    About page text

--- source/about.blade.php
+++ source/about.blade.php
@@ -8,15 +8,29 @@
 @endpush
 
 @section('body')
-    <h1>About</h1>
+    <div class="tw-container tw-w-5/6 tw-mt-6 tw-mx-auto">
+        <h3 class="tw-mt-6">Who Are We?</h3>
 
-    <img src="/assets/img/about.png"
-        alt="About image"
-        class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
+        <p class="tw-mt-4 tw-text-xl tw-font-light">
+            <strong class="tw-font-bold">{{ $page->siteName }}</strong> was founded in 2018 as a private software development organization focused on building interesting, useful, and quirky apps.
+        </p>
 
-    <p class="mb-6">This is where you can give a little more information about yourself or site. If you'd like to change the structure of this page, you can find the file at <code>source/about.blade.php</code></p>
+        <p class="tw-mt-4 tw-text-xl tw-font-light">
+            "We" are a single developer based in Illinois U.S.A, with over 20 years experience in the code-tinkering industry. On this website I shall simply be known as "The Dev".
+        </p>
 
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam possimus earum harum itaque est!</p>
+        <p class="tw-mt-4 tw-text-xl tw-font-light">
+            As long as I can remember, I enjoyed building things. For lack of talent in the arts, I gravitated towards one of the few mediums that allowed me to express my creativity: code.
+        </p>
 
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam!</p>
+        {{--<h3 class="tw-mt-6">Our Mission</h3>
+
+        <p class="tw-mt-4 tw-text-xl tw-font-light">
+            My fascination for product design
+        </p>
+
+        <p class="tw-mt-4 tw-text-xl tw-font-light">
+            In time.
+        </p>--}}
+    </div>
 @endsection
