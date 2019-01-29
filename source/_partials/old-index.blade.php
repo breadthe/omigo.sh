fae8987a6c8ebd30611e0c9741953a6437a3e4c3
breadthe <ferdinand.bismarck@gmail.com>
2019-01-28 21:10:28 -0600

    Replace old index content with Omigo.sh welcome and hero products

---
+++ source/_partials/old-index.blade.php
@@ -0,0 +1,51 @@
+@extends('_layouts.master')
+
+@section('body')
+    @foreach ($posts->where('published', true)->where('featured', true) as $featuredPost)
+        <div class="w-full mb-6">
+            @if ($featuredPost->cover_image)
+                <img src="{{ $featuredPost->cover_image }}" alt="{{ $featuredPost->title }} cover image" class="mb-6">
+            @endif
+
+            <p class="text-md text-grey-dark my-2">
+                {{ $featuredPost->getDate()->format('F j, Y') }}
+            </p>
+
+            <h2 class="font-serif text-4xl mt-0">
+                <a href="{{ $featuredPost->getUrl() }}" title="Read {{ $featuredPost->title }}" class="text-pink-dark hover:text-pink-darker font-extrabold">
+                    {{ $featuredPost->title }}
+                </a>
+            </h2>
+
+            <p class="mt-0 mb-4">{!! $featuredPost->getExcerpt() !!}</p>
+
+            <a href="{{ $featuredPost->getUrl() }}" title="Read - {{ $featuredPost->title }}" class="uppercase tracking-wide mb-4 hover:no-underline text-pink-dark hover:text-pink-darker">
+                Read
+            </a>
+        </div>
+
+        @if (! $loop->last)
+            <hr class="border-b my-6">
+        @endif
+    @endforeach
+
+    {{--@include('_components.newsletter-signup')--}}
+
+    @foreach ($posts->where('published', true)->where('featured', false)->take(6)->chunk(2) as $row)
+        <div class="flex flex-col md:flex-row md:-mx-6">
+            @foreach ($row as $post)
+                <div class="w-full md:w-1/2 md:mx-6">
+                    @include('_components.post-preview-inline')
+                </div>
+
+                @if (! $loop->last)
+                    <hr class="block md:hidden w-full border-b mt-2 mb-6">
+                @endif
+            @endforeach
+        </div>
+
+        @if (! $loop->last)
+            <hr class="w-full border-b mt-2 mb-6">
+        @endif
+    @endforeach
+@stop
