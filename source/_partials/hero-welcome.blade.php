f46184a6b8218a800e5f3a5e2cfe1edb3cc3d2a3
breadthe <ferdinand.bismarck@gmail.com>
2019-01-28 21:11:00 -0600

    Welcome page content and products section

---
+++ source/_partials/hero-welcome.blade.php
@@ -0,0 +1,33 @@
+<section class="hero h-auto py-4 sm:py-32 flex items-center">
+
+    <div class="container mx-auto max-w-xl">
+        <div class="w-full p-2 mb-8">
+            <div class="text-center mb-2">
+                <h1 class="text-3xl sm:text-5xl font-light text-white">SOFTWARE EXPERIMENTS</h1>
+            </div>
+
+            <div class="text-xl sm:text-3xl font-light text-center text-grey-lightest">
+                ... for fun and profit.
+            </div>
+
+            <div class="text-center">
+                <h2 class="text-xl sm:text-3xl font-light text-grey-lightest">
+                    We take ideas and turn them into web software products and SaaS apps. Sometimes we re-invent the wheel but that's OK, because the wheel can always get better.
+                </h2>
+            </div>
+        </div>
+
+        <div class="w-full p-2 mb-8">
+            <div class="text-center mb-2">
+                <h1 class="text-3xl sm:text-5xl font-light text-white">TECHNOLOGY STACK</h1>
+            </div>
+
+            <div class="text-center">
+                <h2 class="text-xl sm:text-3xl font-light text-grey-lightest">
+                    We craft custom solutions using the Laravel PHP framework, VueJS, and TailwindCSS.
+                </h2>
+            </div>
+        </div>
+    </div>
+
+</section>
