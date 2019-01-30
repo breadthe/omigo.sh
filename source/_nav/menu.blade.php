f68d5d0964b759553937de3a3aafcfcae11f99fa
breadthe <ferdinand.bismarck@gmail.com>
2019-01-29 22:55:42 -0600

    Style header nav links dynamically based on location

--- source/_nav/menu.blade.php
+++ source/_nav/menu.blade.php
@@ -1,16 +1,43 @@
 <nav class="hidden lg:flex items-center justify-end text-lg">
-    <a title="{{ $page->siteName }} Blog" href="/blog"
-        class="ml-6 text-grey-darker hover:text-pink-dark {{ $page->isActive('/blog') ? 'active text-pink-dark' : '' }}">
+    <a
+        title="{{ $page->siteName }} Blog"
+        href="/blog"
+        class="
+            ml-6
+            {{
+                $page->belongsTo('/blog') ?
+                'active text-grey-darkest hover:text-grey-darkest border-b-2 border-grey-darkest' :
+                'text-pink-lightest hover:text-white'
+            }}"
+    >
         Blog
     </a>
 
-    <a title="{{ $page->siteName }} About" href="/about"
-        class="ml-6 text-grey-darker hover:text-pink-dark {{ $page->isActive('/about') ? 'active text-pink-dark' : '' }}">
+    <a
+        title="{{ $page->siteName }} About"
+        href="/about"
+        class="
+            ml-6
+            {{
+                $page->belongsTo('/about') ?
+                'active text-white hover:text-white border-b-2 border-white' :
+                ($page->belongsTo('/blog') ? 'text-grey-dark hover:text-grey-darkest': 'text-pink-lightest hover:text-white')
+            }}"
+    >
         About
     </a>
 
-    <a title="{{ $page->siteName }} Contact" href="/contact"
-        class="ml-6 text-grey-darker hover:text-pink-dark {{ $page->isActive('/contact') ? 'active text-pink-dark' : '' }}">
+    <a
+        title="{{ $page->siteName }} Contact"
+        href="/contact"
+        class="
+            ml-6
+            {{
+                $page->belongsTo('/contact') ?
+                'active text-white hover:text-white border-b-2 border-white' :
+                ($page->belongsTo('/blog') ? 'text-grey-dark hover:text-grey-darkest': 'text-pink-lightest hover:text-white')
+            }}"
+    >
         Contact
     </a>
 </nav>
