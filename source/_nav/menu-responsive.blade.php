368f9d7f2ccdc8f560a1b7b121ef86edbe47367e
breadthe <ferdinand.bismarck@gmail.com>
2019-01-29 23:29:31 -0600

    Style responsive header nav dynamically based on location

--- source/_nav/menu-responsive.blade.php
+++ source/_nav/menu-responsive.blade.php
@@ -1,24 +1,46 @@
-<nav id="js-nav-menu" class="nav-menu hidden lg:hidden">
+<nav id="js-nav-menu" class="nav-menu hidden lg:hidden {{ $page->belongsTo('/blog') ? 'bg-grey-lighter' : 'bg-red' }}">
     <ul class="list-reset my-0">
         <li class="pl-4">
             <a
                 title="{{ $page->siteName }} Blog"
                 href="/blog"
-                class="nav-menu__item hover:text-pink-dark {{ $page->isActive('/blog') ? 'active text-pink-dark' : '' }}"
+                class="
+                    nav-menu__item
+                    {{
+                        $page->belongsTo('/blog') ?
+                        'active text-grey-darkest hover:text-grey-darkest font-bold' :
+                        'text-pink-lightest hover:text-white'
+                    }}
+                "
+
             >Blog</a>
         </li>
         <li class="pl-4">
             <a
                 title="{{ $page->siteName }} About"
                 href="/about"
-                class="nav-menu__item hover:text-pink-dark {{ $page->isActive('/about') ? 'active text-pink-dark' : '' }}"
+                class="
+                    nav-menu__item
+                    {{
+                        $page->belongsTo('/about') ?
+                        'active text-white hover:text-white font-bold' :
+                        ($page->belongsTo('/blog') ? 'text-grey-dark hover:text-grey-darkest': 'text-pink-lightest hover:text-white')
+                    }}
+                "
             >About</a>
         </li>
         <li class="pl-4">
             <a
                 title="{{ $page->siteName }} Contact"
                 href="/contact"
-                class="nav-menu__item hover:text-pink-dark {{ $page->isActive('/contact') ? 'active text-pink-dark' : '' }}"
+                class="
+                    nav-menu__item
+                    {{
+                        $page->belongsTo('/contact') ?
+                        'active text-white hover:text-white font-bold' :
+                        ($page->belongsTo('/blog') ? 'text-grey-dark hover:text-grey-darkest': 'text-pink-lightest hover:text-white')
+                    }}
+                "
             >Contact</a>
         </li>
     </ul>
