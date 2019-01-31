6bcd42ee7c4d392326a3602786af0a42b698dc09
breadthe <ferdinand.bismarck@gmail.com>
2019-01-31 00:32:13 -0600

    Post hero image is now displayed based on post metadata

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -18,6 +18,19 @@
         {{ $page->author }}  •  {{ date('F j, Y', $page->date) }}
     </p>
 
+    <section class="w-full">
+        @if($image = $page->image)
+            <img src="{{ $image }}" alt="{{ $page->imageAttribution() }}" title="{{ $page->imageAttribution() }}">
+
+            <small class="block text-center text-xs">
+                Photo by <a href="{{ $page->image_author_url }}">{{ $page->image_author }}</a>
+                @if($page->image_unsplash)
+                    on <a href="https://unsplash.com">Unsplash</a>
+                @endif
+            </small>
+        @endif
+    </section>
+
     <div class="border-b border-pink text-xl text-grey-darkest mb-10 pb-4" v-pre>
         @yield('content')
 
