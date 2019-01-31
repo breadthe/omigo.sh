7f93015724039ef5dca53ef12ca462fa27bf0144
breadthe <ferdinand.bismarck@gmail.com>
2019-01-30 23:51:05 -0600

    Contact form that posts to Netlify

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -16,9 +16,42 @@
 
 @section('body')
 <h3>Get In Touch</h3>
+<p>We'll get back to you ASAP as possible!</p>
 
-<p class="mb-8">
-    Contact info coming soon.
-</p>
+<section class="mb-8">
+    <form
+        name="contact"
+        method="post"
+        data-netlify="true"
+    >
+        <div
+            class="flex justify-start pb-4"
+        >
+            <div class="w-full">
+                <input placeholder="Your Name" type="text" id="name" name="name" />
+            </div>
+        </div>
+        <div
+            class="flex justify-start pb-4"
+        >
+            <div class="w-full">
+                <input placeholder="Your Email" type="email" id="email" name="email" />
+            </div>
+        </div>
+        <div
+            class="flex justify-start pb-4"
+        >
+            <div class="w-full">
+                <textarea placeholder="Message" id="message" name="message"></textarea>
+            </div>
+        </div>
+        <div class="text-right">
+            <button
+                type="submit"
+                class=""
+            >Send</button>
+        </div>
+    </form>
+</section>
 
 @stop
