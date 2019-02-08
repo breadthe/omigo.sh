67c83061b79cf03b6ee59f830e103cbbfdcaf855
breadthe <ferdinand.bismarck@gmail.com>
2019-02-07 20:42:44 -0600

    Center hero image

--- source/_partials/post-hero-image.blade.php
+++ source/_partials/post-hero-image.blade.php
@@ -1,4 +1,4 @@
-<section class="w-full">
+<section class="w-full text-center">
     @if($image = $page->image)
         <img src="{{ $image }}" alt="{{ $page->imageAttribution() }}" title="{{ $page->imageAttribution() }}">
 
