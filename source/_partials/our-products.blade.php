dfe63f7f5868b89a39d4e98c8db5785de9fe9315
breadthe <ferdinand.bismarck@gmail.com>
2019-05-01 20:59:36 -0500

    Remove broken class

--- source/_partials/our-products.blade.php
+++ source/_partials/our-products.blade.php
@@ -7,7 +7,7 @@
         </div>
 
         <div class="mt-1 flex flex-wrap items-start justify-center">
-            <div class="w-5/6 sm:w-1/4 h- p-2 m-4 text-lg text-center">
+            <div class="w-5/6 sm:w-1/4 p-2 m-4 text-lg text-center">
                 <div class="flex items-center justify-center">
                     <img src="/assets/images/sikrt-logo.png" width="30" height="30">&nbsp;
                     <span class="font-bold">Sikrt</span>&nbsp;
@@ -18,7 +18,7 @@
                     Share sensitive information securely via encrypted, short duration, self-destructing URLs.
                 </p>
             </div>
-            <div class="w-5/6 sm:w-1/4 h- p-2 m-4 text-lg text-center text-grey-darkest">
+            <div class="w-5/6 sm:w-1/4 p-2 m-4 text-lg text-center text-grey-darkest">
                 <div class="flex items-center justify-center">
                     <span class="font-bold">AllMySh*</span>&nbsp;
                     &bullet;&nbsp;
@@ -28,7 +28,7 @@
                     Catalog, track, and document your personal possessions.
                 </p>
             </div>
-            <div class="w-5/6 sm:w-1/4 h- p-2 m-4 text-lg text-center text-grey-darkest">
+            <div class="w-5/6 sm:w-1/4 p-2 m-4 text-lg text-center text-grey-darkest">
                 <span class="font-bold">And More...</span>
                 <p class="">
                     Our motto: "There's no shortage of ideas".
